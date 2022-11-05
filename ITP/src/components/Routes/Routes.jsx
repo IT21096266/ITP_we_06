07770b170a012b3aab0a00f710be28166e76f0d3
Glen.N.Anthick <it21096266@my.sliit.lk>
2022-11-05 18:46:36 +0530

    Merging by Glen

--- ITP/src/components/Routes/Routes.jsx
+++ ITP/src/components/Routes/Routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import { AddressForm, AddressList, AddressUpdateForm, Home, LoginPage, SignUpPage, CreateContainer, SpearParts, SupplierForm, SupplierList,
-          SupplierUpdate, StockList, StockForm, StockUpdateForm, StockSearch, Mytickets, Adminticket, TicketForm, 
-          EmployeeForm,EmployeeList,EmployeeUpdate,EmployeeSalary } from '../../pages'
+import { AddressForm, AddressList, AddressUpdateForm, Home, LoginPage, SignUpPage, CreateContainer, SpearParts, OnlineItemList,
+          SupplierForm, SupplierList,SupplierUpdate, StockList, StockForm, StockUpdateForm, StockSearch, Mytickets, 
+          Adminticket, TicketForm, EmployeeForm, EmployeeList, EmployeeUpdate, EmployeeSalary } from '../../pages'
 import { MainContainer } from '../index'
 
 const AppRoutes = () => {
@@ -13,15 +13,15 @@ const AppRoutes = () => {
         <>
           <Route path="/home" element={<Home />} />
           <Route path="/spearParts" element={<SpearParts />} />
-          <Route path="/spearParts" element={<SpearParts />} />
           <Route path="/createItem" element={<CreateContainer />} />
           <Route path="/address" element={<AddressForm />} />
           <Route path="/addressUpdate" element={<AddressUpdateForm  />} />
           <Route path="/addressList" element={<AddressList />} />
+          <Route path="/onlineItemList" element={<OnlineItemList />} />
+          <Route path="addressList/addressUpdate/:addressID" element={<AddressUpdateForm  />} />
           <Route path="/Mytickets" element={<Mytickets />} />
           <Route path="/Adminticket" element={<Adminticket />} />
           <Route path="/TicketForm" element={<TicketForm />} />
-          <Route path="addressList/addressUpdate/:addressID" element={<AddressUpdateForm  />} />
           <Route path="/employee" element={<EmployeeForm />} />
           <Route path="/employeeList" element={<EmployeeList  />} />
           <Route path="/employeeUpdate" element={<EmployeeUpdate  />} />
